b4c8079614d36396516cc831a40e106920c770fd
Tusharkant21 <64279043+Tusharkant21@users.noreply.github.com>
2022-12-09 17:19:57 +0530

    Pizza Billing code

--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -5,10 +5,33 @@ public class Pizza {
     private int price;
     private Boolean isVeg;
     private String bill;
-
+    private int extraCheesePrice;
+    private  int extraToppingPrice;
+    private int takeAwayPrice;
+    private boolean isCheesAdded;
+    private boolean isToppingAdded;
+    private boolean isBillGenerated;
+    private boolean isTakeAway;
     public Pizza(Boolean isVeg){
         this.isVeg = isVeg;
+
         // your code goes here
+        this.isCheesAdded=false;
+        this.isToppingAdded=false;
+        this.isTakeAway=false;
+        this.isBillGenerated=false;
+        this.takeAwayPrice=20;
+        this.extraCheesePrice=80;
+        if(isVeg==true){
+            this.price=300;
+            this.extraToppingPrice=70;
+
+        }
+        else{
+            this.price=400;
+            this.extraToppingPrice=120;
+        }
+        this.bill="Base Price of Pizza: "+this.price+"\n";
     }
 
     public int getPrice(){
@@ -17,18 +40,36 @@ public class Pizza {
 
     public void addExtraCheese(){
         // your code goes here
+        if(isCheesAdded==false) {
+            this.price = this.price + this.extraCheesePrice;
+            this.isCheesAdded=true;
+        }
     }
 
     public void addExtraToppings(){
         // your code goes here
+        if(isToppingAdded==false) {
+            this.price = this.price + this.extraToppingPrice;
+            this.isToppingAdded=true;
+        }
     }
 
     public void addTakeaway(){
         // your code goes here
+        if(isTakeAway==false){
+            this.price=this.price+this.takeAwayPrice;
+            isTakeAway=true;
+        }
     }
 
     public String getBill(){
         // your code goes here
+        if(this.isBillGenerated==false){
+            this.isBillGenerated=true;
+            this.bill=this.bill+this.price;
+
+        }
+
         return this.bill;
     }
 }
